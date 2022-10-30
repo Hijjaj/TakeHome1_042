da5ea0f844709763af5a07a8ae1f2f66d6b5d19c
Hijjaj <hijjaj.refza.ft20@mail.umy.ac.id>
2022-10-30 21:24:11 +0700

    index.html

---
+++ src/main/java/cemilan/a/cemilan2/Cemilan2Application.java
@@ -0,0 +1,13 @@
+package cemilan.a.cemilan2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cemilan2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cemilan2Application.class, args);
+	}
+
+}
---
+++ src/test/java/cemilan/a/cemilan2/Cemilan2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cemilan.a.cemilan2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cemilan2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
